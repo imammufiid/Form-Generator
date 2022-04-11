8c3696063c0625c5a58c6e29b28fc01267ef572d
imufiid <imufiid98@gmail.com>
2022-04-11 15:13:21 +0700

    Created edit text multiple

--- FormGenerator/src/main/java/id/mufiid/formgenerator/formgenerator/function/ViewChecker.kt
+++ FormGenerator/src/main/java/id/mufiid/formgenerator/formgenerator/function/ViewChecker.kt
@@ -3,6 +3,7 @@ package id.mufiid.formgenerator.formgenerator.function
 import android.content.Context
 import android.widget.Toast
 import id.mufiid.formgenerator.formgenerator.views.EditTextController
+import id.mufiid.formgenerator.formgenerator.views.EditTextMultipleController
 
 class ViewChecker {
     companion object {
@@ -25,10 +26,33 @@ class ViewChecker {
             if (!editTextController.isFilled && !editTextController.isNullable) {
                 editTextController.view?.requestFocus()
                 Toast.makeText(
-                    context, errorMessagePrefix + " " + editTextController.title + " " + errorMessagePostfix,
+                    context,
+                    errorMessagePrefix + " " + editTextController.title + " " + errorMessagePostfix,
                     Toast.LENGTH_SHORT
                 ).show()
                 return false
+            }
+            return true
+        }
+
+        fun isFilled(
+            editTextMultipleController: EditTextMultipleController,
+            context: Context
+        ): Boolean {
+            for (key in editTextMultipleController.editTextList?.keys!!) {
+                if (
+                    editTextMultipleController.editTextList?.get(key)?.isFilled == false &&
+                    editTextMultipleController.editTextList?.get(key)?.isNullable == false
+                ) {
+                    editTextMultipleController.editTextList?.get(key)?.view?.requestFocus()
+                    Toast.makeText(
+                        context, errorMessagePrefix
+                                + " " + editTextMultipleController.editTextList?.get(key)?.title
+                                + " " + errorMessagePostfix,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return false
+                }
             }
             return true
         }
