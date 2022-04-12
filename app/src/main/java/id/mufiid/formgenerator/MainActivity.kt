f2bc3d915c7b2db8acdcf2927b4cc6ab9a920957
imufiid <imufiid98@gmail.com>
2022-04-12 12:56:23 +0700

    Added some example form generator

--- app/src/main/java/id/mufiid/formgenerator/MainActivity.kt
+++ app/src/main/java/id/mufiid/formgenerator/MainActivity.kt
@@ -1,12 +1,13 @@
 package id.mufiid.formgenerator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import id.mufiid.formgenerator.databinding.ActivityMainBinding
+import id.mufiid.formgenerator.formgenerator.model.SpinnerData
 import id.mufiid.formgenerator.formgenerator.utils.Mode
-import id.mufiid.formgenerator.formgenerator.views.ButtonController
-import id.mufiid.formgenerator.formgenerator.views.EditTextController
+import id.mufiid.formgenerator.formgenerator.views.*
 
 class MainActivity : AppCompatActivity() {
     private var _bind: ActivityMainBinding? = null
@@ -28,10 +29,123 @@ class MainActivity : AppCompatActivity() {
         /**
          * Attach Edit Text View
          */
-        val editTextId = editTextBuilder.clone().apply {
-            title = "ID"
+        val searchEditText = editTextBuilder.clone().apply {
+            title = "Edit Text Search"
             mode = Mode.SEARCH
         }.create()
+
+        /**
+         * Attach Edit Text View Mode.GENERAL
+         */
+        editTextBuilder.clone().apply {
+            title = "Edit Text General"
+            mode = Mode.GENERAL
+        }.create()
+
+        /**
+         * Attach Edit Text View Mode.GENERAL
+         */
+        editTextBuilder.clone().apply {
+            title = "Edit Text Min Lines"
+            mode = Mode.GENERAL
+            setMinLines(4)
+        }.create()
+
+        /**
+         * Attach Edit Text Date Picker
+         */
+        editTextBuilder.clone().setInputType(InputType.TYPE_CLASS_DATETIME)
+            .setTitle("Edit Text Date Picker").setDateFormat("dd-MM-yyyy")
+            .setTitleColorResource(id.mufiid.formgenerator.formgenerator.R.color.dark_grey)
+            .create()
+
+        /**
+         * Attach Edit Text Time Picker
+         */
+        editTextBuilder.clone().setInputType(InputType.TYPE_DATETIME_VARIATION_TIME)
+            .setTitle("Edit Text Time Picker")
+            .setTitleColorResource(id.mufiid.formgenerator.formgenerator.R.color.dark_grey)
+            .create()
+
+        /**
+         * Attach Multiple Edit Text
+         */
+        val edtList: HashMap<String?, EditTextController?> = LinkedHashMap()
+        val edtItemBuilder = EditTextController.Builder(this)
+        edtList["No"] =
+            edtItemBuilder.clone().setTitle("No").setInputType(InputType.TYPE_CLASS_NUMBER).create()
+        edtList["Zip"] =
+            edtItemBuilder.clone().setTitle("Zip").setInputType(InputType.TYPE_CLASS_NUMBER)
+                .create()
+        EditTextMultipleController.Builder(this, edtList)
+            .setFormLayout(_bind?.formLayout).setMargin(50).create()
+
+        /**
+         * Attach Spinner
+         */
+        val cityList: ArrayList<SpinnerData> = ArrayList()
+        cityList.add(SpinnerData(1, "1", "Malang"))
+        cityList.add(SpinnerData(2, "2", "Surabaya", true))
+        cityList.add(SpinnerData(3, "3", "Jakarta"))
+        SpinnerController.Builder(this).apply {
+            title = "City"
+            setItem(cityList)
+            setDefaultSelectedValue("Jakarta")
+        }.setFormLayout(_bind?.formLayout).create()
+
+        /**
+         * Attach Text View
+         */
+        TextViewController.Builder(this)
+            .apply {
+                title = "Text View Title"
+                content = "Text View Content"
+            }
+            .setFormLayout(_bind?.formLayout)
+            .create()
+
+        /**
+         * Attach Checkbox
+         */
+        CheckBoxController.Builder(this).apply {
+            title = "Checkbox"
+            checkBoxItem = arrayListOf("1", "2", "3")
+            formLayout = _bind?.formLayout
+        }.create()
+
+        /**
+         * Attach Radio Button
+         */
+
+        //create radio button
+        RadioButtonController.Builder(this)
+            .setTitle("Radio Button").setOptionList(arrayOf("Male", "Female"))
+            .setFormLayout(_bind?.formLayout).setSelected("Female").create()
+
+        /**
+         * Attach Horizontal Layout
+         */
+        val rtEditText = EditTextController.Builder(this).apply {
+            title = "RT"
+        }.create()
+        val rwEditText = EditTextController.Builder(this).apply {
+            title = "RW"
+        }.create()
+        val button = ButtonController.Builder(this).setText("Check").create()
+        val rsEditText = EditTextController.Builder(this).apply {
+            title = "RW"
+        }.create()
+        val horizontalController = HorizontalLayoutController(this, 0, 20)
+        horizontalController.addView(rtEditText.view, false)
+        horizontalController.addView(rwEditText.view, false)
+        horizontalController.addView(button.getView(), true)
+        horizontalController.addView(rsEditText.view, false)
+        _bind?.formLayout?.addView(horizontalController.getView())
+
+        EditTextController.Builder(this).apply {
+            title = "RW"
+        }.setFormLayout(_bind?.formLayout).create()
+
 
         /**
          * Attach Button View
@@ -39,20 +153,20 @@ class MainActivity : AppCompatActivity() {
         _bind?.formLayout?.let {
             ButtonController.Builder(this)
                 .setFormLayout(it).apply {
-                text = "My Button"
-            }
+                    text = "My Button"
+                }
                 .setOnClickListener {
                     Toast.makeText(this, "Button Controller", Toast.LENGTH_SHORT).show()
                 }
                 .create()
         }
 
-        editTextId.setOnClickSearchListener {
+        searchEditText.setOnClickSearchListener {
             Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
 
         _bind?.btnSubmit?.setOnClickListener {
-            Toast.makeText(this, editTextId.value, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, searchEditText.value, Toast.LENGTH_SHORT).show()
         }
     }
 
